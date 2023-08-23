c1e0581c2d7700f0ceef10542e4666a01a83551a
zzagir <zaynalov005@mail.ru>
2023-08-23 23:12:49 +0300

    5

---
+++ assets/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import styles from './Navigation.module.scss'
+
+const Navigation: FC = () => {
+	return <div>Navigation</div>
+}
+
+export default Navigation
--- assets/components/screens/home/Home.tsx
+++ assets/components/screens/home/Home.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 import styles from './Home.module.scss'
 import { IHome } from './home.interface'
+import Layout from '../../layout/Layout'
 
 const Home: FC<IHome> = () => {
-	return <div>Home</div>
+	return <Layout>Home</Layout>
 }
 
 export default Home
