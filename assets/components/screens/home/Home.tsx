15ad67771ca6936de778a5f1034c133c34a99698
zzagir <zaynalov005@mail.ru>
2023-08-23 22:59:41 +0300

    5

---
+++ assets/components/screens/home/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import styles from './Home.module.scss'
+import { IHome } from './home.interface'
+
+const Home: FC<IHome> = () => {
+	return <div>Home</div>
+}
+
+export default Home
